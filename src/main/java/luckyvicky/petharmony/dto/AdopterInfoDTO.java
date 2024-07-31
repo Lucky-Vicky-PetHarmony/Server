4167db554ea0edc64345c8c42a34c20dd7ded78c
dajinYang <didekwls0104@naver.com>
2024-08-01 04:36:28 +0900

    feat: adopter_info테이블에 대한 DTO, Entity, Repository 추가

---
+++ src/main/java/luckyvicky/petharmony/dto/AdopterInfoDTO.java
@@ -0,0 +1,89 @@
+package luckyvicky.petharmony.dto;
+
+import java.util.Date;
+
+public class AdopterInfoDTO {
+
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private Date birthDate;
+    private String phoneNumber;
+    private String gender;
+    private String address;
+    private String favoriteWord;
+
+    // Getter 및 Setter 메소드
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getFavoriteWord() {
+        return favoriteWord;
+    }
+
+    public void setFavoriteWord(String favoriteWord) {
+        this.favoriteWord = favoriteWord;
+    }
+}
