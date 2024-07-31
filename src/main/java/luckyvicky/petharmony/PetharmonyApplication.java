e23f144a477cdb102896eb47cb208b16359c5af1
dajinYang <didekwls0104@naver.com>
2024-08-01 04:07:21 +0900

    first commit

---
+++ src/main/java/luckyvicky/petharmony/PetharmonyApplication.java
@@ -0,0 +1,13 @@
+package luckyvicky.petharmony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetharmonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetharmonyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luckyvicky/petharmony/PetharmonyApplicationTests.java
@@ -0,0 +1,13 @@
+package luckyvicky.petharmony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetharmonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
