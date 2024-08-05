61fbd2d39be9d585950b1d56d6c6b70ad028e719
CHAERIM LEE <leechaerim214@gmail.com>
2024-08-06 03:51:39 +0900

    feat: User 테이블에 대한 Entity, DTO, Repository

---
+++ src/main/java/luckyvicky/petharmony/entity/User.java
@@ -0,0 +1,40 @@
+package luckyvicky.petharmony.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;                           // 회원 ID
+
+    @Column(length = 100, nullable = false)
+    private String userName;                       // 회원 이름
+
+    @Column(length = 100, nullable = false)
+    private String email;                         // 회원 이메일
+
+    @Column(length = 100, nullable = false)
+    private String password;                      // 회원 비밀번호
+
+    @Column(length = 100, nullable = false)
+    private String phone;                         // 회원 전화번호
+
+    @Column(length = 500)
+    private String address;                       // 회원 주소
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createDate;             // 회원 가입일자
+
+}
