7da2b8beda3c2666a150b74a4c9f025fde1c6eaa
Gaannini <0107162a@gmail.com>
2024-08-09 01:18:16 +0900

    feat: board, comment, image, petlike 레포지토리 생성

---
+++ src/main/java/luckyvicky/petharmony/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package luckyvicky.petharmony.repository;
+
+import luckyvicky.petharmony.entity.board.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
