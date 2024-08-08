013a7b9f4e9158d67f2013e0bea506dc59b0ef8b
CHAERIM LEE <leechaerim214@gmail.com>
2024-08-09 04:45:12 +0900

    feat: CustomUserDetailsService username 대신 email을 사용하여 사용자 검색

--- src/main/java/luckyvicky/petharmony/repository/UserRepository.java
+++ src/main/java/luckyvicky/petharmony/repository/UserRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    // Spring Security에서 이메일을 사용해 인증 처리
+    Optional<User> findByEmail(String email);
 
     // 세션 범위를 벗어난 엔팉티 지연 로딩된 컬렉션 접근 방지 (필요한 관계 즉시 로딩)
     @EntityGraph(attributePaths = {"userWords"})
