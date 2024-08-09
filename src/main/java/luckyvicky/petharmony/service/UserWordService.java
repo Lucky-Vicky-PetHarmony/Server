eae1938f141c4a7a551cab435e6daa5d1662070c
dajinYang <didekwls0104@naver.com>
2024-08-09 18:41:20 +0900

    refactor: UserWordService에서 엔티티 대신 DTO 사용

--- src/main/java/luckyvicky/petharmony/service/UserWordService.java
+++ src/main/java/luckyvicky/petharmony/service/UserWordService.java
@@ -1,9 +1,7 @@
 package luckyvicky.petharmony.service;
 
 import luckyvicky.petharmony.dto.UserWordDTO;
-import luckyvicky.petharmony.entity.User;
 import luckyvicky.petharmony.entity.UserWord;
-import luckyvicky.petharmony.entity.Word;
 import luckyvicky.petharmony.repository.UserRepository;
 import luckyvicky.petharmony.repository.UserWordRepository;
 import luckyvicky.petharmony.repository.WordRepository;
@@ -17,15 +15,12 @@ import java.util.stream.Collectors;
 @Service
 public class UserWordService {
 
-    // UserRepository 주입
     @Autowired
     private UserRepository userRepository;
 
-    // WordRepository 주입
     @Autowired
     private WordRepository wordRepository;
 
-    // UserWordRepository 주입
     @Autowired
     private UserWordRepository userWordRepository;
 
@@ -36,42 +31,28 @@ public class UserWordService {
      */
     @Transactional
     public void saveUserWords(List<UserWordDTO> userWordDTOs) {
-        // 각 UserWordDTO를 순회하며 처리
-        for (UserWordDTO userWordDTO : userWordDTOs) {
-            // 사용자 ID를 사용하여 User 엔티티를 조회
-            User user = userRepository.findById(userWordDTO.getUserId())
-                    .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
-            // 단어 ID를 사용하여 Word 엔티티를 조회
-            Word word = wordRepository.findById(userWordDTO.getWordId())
-                    .orElseThrow(() -> new RuntimeException("단어를 찾을 수 없습니다."));
+        List<UserWord> userWords = userWordDTOs.stream().map(dto -> {
+            return new UserWord(
+                    null,
+                    userRepository.findById(dto.getUserId())
+                            .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다.")),
+                    wordRepository.findById(dto.getWordId())
+                            .orElseThrow(() -> new RuntimeException("단어를 찾을 수 없습니다."))
+            );
+        }).collect(Collectors.toList());
 
-            // 새로운 UserWord 엔티티를 생성
-            UserWord userword = new UserWord();
-            // UserWord 엔티티에 User 엔티티를 설정
-            userword.assignUser(user);
-            // UserWord 엔티티를 데이터베이스에 저장
-            userword.assignWord(word);
-
-            // UserWord 엔티티를 데이터베이스에 저장
-            userWordRepository.save(userword);
-        }
+        userWordRepository.saveAll(userWords);
     }
 
     /**
      * 특정 사용자가 선택한 모든 선호 단어 목록을 조회하는 메서드
+     *
      * @param userId 사용자의 ID
      * @return 사용자가 선택한 UserWordDTO 목록
      */
     public List<UserWordDTO> getUserWords(Long userId) {
-        // 사용자 ID를 사용하여 User 엔티티를 조회합니다.
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
-        // User 엔티티의 userWords 리스트를 UserWordDTO 리스트로 변환하여 반환합니다.
-        return user.getUserWords().stream()
-                .map(userWord -> new UserWordDTO(
-                        userWord.getUser().getUserId(),
-                        userWord.getWord().getWordId()
-                ))
+        return userWordRepository.findByUserUserId(userId).stream()
+                .map(userWord -> new UserWordDTO(userWord.getUser().getUserId(), userWord.getWord().getWordId()))
                 .collect(Collectors.toList());
     }
 }
