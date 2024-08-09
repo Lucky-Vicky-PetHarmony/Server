8f1d88ddcc22d1d869638d0162d38583a9aa8397
CHAERIM LEE <leechaerim214@gmail.com>
2024-08-09 18:37:00 +0900

    feat: JWT를 사용한 인증 및 권한 부여 - JwtTokenProvider.java : JWT 토큰을 생성하고, 유효성 검사, 토큰에서 사용자 정보 추출 - JwtAuthenticationFilter : HTTP 요청마다 JWT 토큰을 검증하고, 해당 토큰에 인증 처리하는 필터

---
+++ src/main/java/luckyvicky/petharmony/security/JwtTokenProvider.java
@@ -0,0 +1,51 @@
+package luckyvicky.petharmony.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    private static final long JWT_EXPIRATION_MS = 86400000L;
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + JWT_EXPIRATION_MS);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(key)
+                .compact();
+    }
+
+    public String getUsernameFromJWT(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+}
