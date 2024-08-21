5e22366e21f629949b1b1baf5dd4470c29886e3b
CHAERIM LEE <leechaerim214@gmail.com>
2024-08-22 00:28:18 +0900

    chore: OAuth2 기능을 사용하지 않아 코드 삭제(추후 리팩토링 필요)

--- src/main/java/luckyvicky/petharmony/security/SecurityConfig.java
+++ src/main/java/luckyvicky/petharmony/security/SecurityConfig.java
@@ -1,44 +1,22 @@
 package luckyvicky.petharmony.security;
 
-import lombok.extern.log4j.Log4j2;
-import luckyvicky.petharmony.dto.user.KakaoLogInResponseDTO;
-import luckyvicky.petharmony.entity.User;
-import luckyvicky.petharmony.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.Map;
-import java.util.Optional;
-
-@Log4j2
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final ClientRegistrationRepository clientRegistrationRepository;
-    private final UserRepository userRepository;
-    private final JwtTokenProvider jwtTokenProvider;
-
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, ClientRegistrationRepository clientRegistrationRepository, UserRepository userRepository, JwtTokenProvider jwtTokenProvider) {
-        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-        this.clientRegistrationRepository = clientRegistrationRepository;
-        this.userRepository = userRepository;
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -50,64 +28,16 @@ public class SecurityConfig {
                         .requestMatchers("/api/user/**").hasRole("USER")  // 사용자 엔드포인트
                         .anyRequest().authenticated()  // 그 외 모든 요청은 인증 필요
                 )
-                .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/oauth")
-                        .defaultSuccessUrl("/", true)
-                        .failureUrl("/oauth?error=true")
-                        .userInfoEndpoint(userInfo -> userInfo
-                                .userService(oAuth2UserService(clientRegistrationRepository)))
-                )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
-    // Spring Security의 OAuth2 로그인 플로우에 따라 카카오에서 인증을 완료한 후 사용자 정보 처리
-    // 현재 사용하고 있지 않음 (수정 예정)
-    // 현재는 UserService에서 Spring Security와는 별도로 카카오 API에 직접 요청을 보내고 응답 처리
-    @Bean
-    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(ClientRegistrationRepository clientRegistrationRepository) {
-        DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
-        return request -> {
-            OAuth2User oAuth2User = delegate.loadUser(request);
-            Map<String, Object> attributes = oAuth2User.getAttributes();
-
-            String kakaoId = String.valueOf(attributes.get("id"));
-            Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
-
-            String email = (String) kakaoAccount.get("email");
-            String userName = (String) kakaoAccount.get("name");
-            String phone = (String) kakaoAccount.get("phone_number");
-
-            Optional<User> optionalUser = userRepository.findByKakaoId(kakaoId);
-            User user = optionalUser.orElseGet(() -> {
-                User newUser = User.builder()
-                        .userName(userName)
-                        .email(email)
-                        .phone(phone)
-                        .role(Role.USER)
-                        .userState(UserState.ACTIVE)
-                        .kakaoId(kakaoId)
-                        .build();
-                return userRepository.save(newUser);
-            });
-
-            String jwtToken = jwtTokenProvider.generateToken(
-                    new UsernamePasswordAuthenticationToken(
-                            user.getEmail(),
-                            "kakao#password"
-                    )
-            );
-
-            KakaoLogInResponseDTO response = new KakaoLogInResponseDTO(
-                    jwtToken,
-                    user.getEmail(),
-                    user.getUserName(),
-                    user.getRole().toString()
-            );
-
-            return oAuth2User;
-        };
-    }
+    /*
+       현재 Spring Security와 통합된 OAuth2 로그인 기능을 사용하지 않고
+       UserServeImpl 클래스에서 RestTemplate을 사용하여
+       직접 Kako API에 요청하여 JWT 토큰을 발급하는 방식으로 구현했다.
+       추후 리팩토링 필요 (기록 차원에 주석 남김)
+    */
 
     @Bean
     public PasswordEncoder passwordEncoder() {
