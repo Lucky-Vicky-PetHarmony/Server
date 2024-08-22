f608a5853989e46b9b459eb6c782344e3508e3ab
CHAERIM LEE <leechaerim214@gmail.com>
2024-08-23 06:06:21 +0900

    fix: 현재 인증된 사용자가 PIN한 게시물들을 조회하는 메서드 : getPinPosts()

--- src/main/java/luckyvicky/petharmony/repository/BoardPinRepository.java
+++ src/main/java/luckyvicky/petharmony/repository/BoardPinRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BoardPinRepository extends JpaRepository<BoardPin, Long> {
@@ -15,4 +16,6 @@ public interface BoardPinRepository extends JpaRepository<BoardPin, Long> {
     Optional<BoardPin> findByBoard_BoardIdAndUser_UserId(Long boardId, Long userId);
 
     Integer countByBoard_BoardId(Long boardId);
+    // 사용자 ID에 해당하는 PIN 게시물들을 조회하는 메서드
+    List<BoardPin> findByUser_UserId(Long userId);
 }
