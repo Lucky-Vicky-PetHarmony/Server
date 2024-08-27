c934e4bd3f08b5eb5368ca8b3de920d9b92fc55a
CHAERIM LEE <leechaerim214@gmail.com>
2024-08-27 18:02:42 +0900

    chore: 기존 DTO 수정

--- src/main/java/luckyvicky/petharmony/dto/main/SlideResponseDTO.java
+++ src/main/java/luckyvicky/petharmony/dto/main/SlideResponseDTO.java
@@ -7,13 +7,13 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SlideResponseDTO {
-    private String desertionNo;     // pet_info 테이블 id
+    private String desertionNo;
 
-    private String popFile;         // 이미지 경로
+    private String popFile;
 
-    private String orgNm;           // 보호 지역
+    private String noticeNo;
 
-    private String sexCd;           // 성별
+    private String sexCd;
 
-    private String age;             // 나이
+    private String age;
 }
