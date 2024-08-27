25d02bee8333ae32b275349060907815ff798c45
CHAERIM LEE <leechaerim214@gmail.com>
2024-08-27 12:16:21 +0900

    feat: [마이페이지] > 관심있는 입양 동물 DTO

---
+++ src/main/java/luckyvicky/petharmony/dto/mypage/MyInterestedPetDTO.java
@@ -0,0 +1,29 @@
+package luckyvicky.petharmony.dto.mypage;
+
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyInterestedPetDTO {
+    private String desertionNo;     // pet_info 테이블 id
+
+    private String popFile;         // 이미지 경로
+
+    private List<String> words;     // 단어
+
+    private String kindCd;          // 종
+
+    private String sexCd;           // 성별
+
+    private String age;             // 나이
+
+    private String weight;          // 몸무게
+
+    private String orgNm;           // 보호 지역
+
+    private String neuterYn;        // 중성화 여부
+}
